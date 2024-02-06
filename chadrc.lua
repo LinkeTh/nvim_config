d4f495a442158cda28ca823506a25caf20b51aed
Thomas Linke <t.linke@pripares.com>
2024-02-06 16:11:33 +0100

    feature: add highlights

--- chadrc.lua
+++ chadrc.lua
@@ -1,6 +1,14 @@
----@type ChadrcConfig 
+---@type ChadrcConfig
 local M = {}
-M.ui = {theme = 'catppuccin'}
+
+local highlights = require "custom.highlights"
+
+M.ui = {
+    theme = "tokyonight",
+    theme_toggle = { "catppuccin", "tokyonight" },
+    hl_override = highlights.override,
+    hl_add = highlights.add,
+}
 M.plugins = "custom.plugins"
 M.mappings = require "custom.mappings"
 return M
